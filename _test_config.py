6211b561d8c49a8a3cb59955b20f87e11750e4df
Raverbury <smurfjp198@gmail.com>
2022-12-15 19:19:35 +0700

    restructure test classes, move param to config

--- _test_config.py
+++ _test_config.py
@@ -11,8 +11,8 @@ class Browser():
 class TestConfig():
     """A class to store test configurations"""
 
-    browser = Browser.FIREFOX
-    """<<EDITABLE>> The browser used to carry out tests\n
+    browser = Browser.EDGE
+    """<<DEPRECATED>> The default browser used to carry out tests\n
     Supported values: ["firefox", "chrome", "edge"] (or just use the Browser enums)"""
 
     base_url = "http://localhost/OnlinePizzaDelivery"
@@ -26,6 +26,14 @@ class TestConfig():
     """<<EDITABLE>> The username of the database used in the application"""
     db_password = ""
     """<<EDITABLE>> The password of the database used in the application"""
+
+    test_parameters = "browser,small_screen_size"
+    """The test parameters used to populate the parametrize decorator"""
+    test_values = [
+        (Browser.EDGE, False),
+        (Browser.EDGE, True)
+    ]
+    """<<EDITABLE>> The test values used in the parametrize decorator"""
 
     @staticmethod
     def get_default_browser():
@@ -44,4 +52,6 @@ class TestConfig():
             return None
         if small_screen_size:
             driver.set_window_size(360, 640)
+        else:
+            driver.maximize_window()
         return driver
